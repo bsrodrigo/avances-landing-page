ba1474328d4870f4c4f12a0610db6f87ba5ce4f2
bsrodrigo <rodrigobs.2708@gmail.com>
2022-09-16 20:23:22 -0300

    feat(core): add organism header

---
+++ src/modules/core/presenters/components/organisms/header/index.tsx
@@ -0,0 +1,22 @@
+import { Typography } from "@mui/material";
+import {
+  Breadcrumbs,
+  IBreadcrumb,
+} from "@/modules/core/presenters/components/molecules";
+
+interface IHeader {
+  title: string;
+  breadcrumbs: IBreadcrumb[];
+}
+
+export const Header: React.FC<IHeader> = ({ breadcrumbs, title }) => {
+  return (
+    <div>
+      <Typography variant="h4" color={(theme) => theme.palette.grey[600]}>
+        {title}
+      </Typography>
+
+      <Breadcrumbs breadcrumbs={breadcrumbs} />
+    </div>
+  );
+};
